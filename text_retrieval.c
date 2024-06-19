ba0ae3cd7aedce549e467421296d472ae035447e
GuilhermeCassFerreira <guilacass.20@gmail.com>
2024-06-18 23:48:55 -0300

    envio de str complexa

--- text_retrieval.c
+++ text_retrieval.c
@@ -3,6 +3,11 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_WORDS 100 // Número máximo de palavras na frase de busca
+#define MAX_WORD_LENGTH 256 // Comprimento máximo de cada palavra
+#define MAX_FILES 100
+#define MAX_FILENAME_LENGTH 256
+
 // Função para contar ocorrências de uma palavra em um arquivo
 int count_occurrences(const char* filename, const char* word) {
     FILE *file = fopen(filename, "r");
@@ -20,46 +25,68 @@ int count_occurrences(const char* filename, const char* word) {
     return count; // Retorna o número de ocorrências
 }
 
+// Função para dividir a frase em palavras
+void split_phrase(char* phrase, char words[MAX_WORDS][MAX_WORD_LENGTH], int* num_words) {
+    char* token = strtok(phrase, " ");
+    *num_words = 0;
+    while (token != NULL) {
+        strncpy(words[*num_words], token, MAX_WORD_LENGTH);
+        words[*num_words][MAX_WORD_LENGTH - 1] = '\0'; // Garantir terminação correta da string
+        (*num_words)++;
+        token = strtok(NULL, " ");
+    }
+}
+
 int main(int argc, char** argv) {
     int rank, size, i;
-
-    if (argc < 2) { // Verifica se foi passada a palavra de busca como argumento
-        if (rank == 0) {
-            fprintf(stderr, "Uso: %s <palavra_de_busca>\n", argv[0]);
-        }
-        return 1; // Encerra o programa se não houver palavra de busca
-    }
 
     MPI_Init(&argc, &argv); // Inicializa o ambiente MPI
     MPI_Comm_rank(MPI_COMM_WORLD, &rank); // Obtém o rank do processo
     MPI_Comm_size(MPI_COMM_WORLD, &size); // Obtém o número total de processos
 
-    const int MAX_FILES = 100;
-    const int MAX_FILENAME_LENGTH = 256;
+    if (argc < 2) { // Verifica se foi passada a frase de busca como argumento
+        if (rank == 0) {
+            fprintf(stderr, "Uso: %s <frase_de_busca>\n", argv[0]);
+        }
+        MPI_Finalize();
+        return 1; // Encerra o programa se não houver frase de busca
+    }
 
-    char search_word[256];
-    strncpy(search_word, argv[1], 256); // Copia a palavra de busca
-    search_word[255] = '\0'; // Garantir terminação correta da string
+    char search_phrase[1024];
+    strncpy(search_phrase, argv[1], 1024); // Copia a frase de busca
+    search_phrase[1023] = '\0'; // Garantir terminação correta da string
+
+    char words[MAX_WORDS][MAX_WORD_LENGTH];
+    int num_words;
+
+    // Processo mestre divide a frase em palavras e distribui
+    if (rank == 0) {
+        split_phrase(search_phrase, words, &num_words);
+
+        // Envia a quantidade de palavras e a lista de palavras para todos os processos
+        MPI_Bcast(&num_words, 1, MPI_INT, 0, MPI_COMM_WORLD);
+        MPI_Bcast(words, num_words * MAX_WORD_LENGTH, MPI_CHAR, 0, MPI_COMM_WORLD);
+    } else {
+        // Processos réplicas recebem a quantidade de palavras e a lista de palavras
+        MPI_Bcast(&num_words, 1, MPI_INT, 0, MPI_COMM_WORLD);
+        MPI_Bcast(words, num_words * MAX_WORD_LENGTH, MPI_CHAR, 0, MPI_COMM_WORLD);
+    }
 
     char filenames[MAX_FILES][MAX_FILENAME_LENGTH];
     int num_files = 0;
 
     if (rank == 0) { // Processo mestre
         // Especifica os caminhos dos arquivos a serem buscados
-        strcpy(filenames[num_files++], "caminho/para/o/file1.txt");
-        strcpy(filenames[num_files++], "caminho/para/o/file2.txt");
+        strcpy(filenames[num_files++], "/home/bridge/MPI_TextRetrieval_ScatterGather/file1.txt");
+        strcpy(filenames[num_files++], "/home/bridge/MPI_TextRetrieval_ScatterGather/file2.txt");
         // Adicionar mais arquivos conforme necessário
 
-        // Envia a palavra de busca para todos os processos
-        MPI_Bcast(search_word, 256, MPI_CHAR, 0, MPI_COMM_WORLD);
-        
         // Envia o número de arquivos para todos os processos
         MPI_Bcast(&num_files, 1, MPI_INT, 0, MPI_COMM_WORLD);
         // Envia os nomes dos arquivos para todos os processos
         MPI_Bcast(filenames, num_files * MAX_FILENAME_LENGTH, MPI_CHAR, 0, MPI_COMM_WORLD);
     } else {
-        // Processos réplicas recebem a palavra de busca, número de arquivos e nomes dos arquivos
-        MPI_Bcast(search_word, 256, MPI_CHAR, 0, MPI_COMM_WORLD);
+        // Processos réplicas recebem o número de arquivos e nomes dos arquivos
         MPI_Bcast(&num_files, 1, MPI_INT, 0, MPI_COMM_WORLD);
         MPI_Bcast(filenames, num_files * MAX_FILENAME_LENGTH, MPI_CHAR, 0, MPI_COMM_WORLD);
     }
@@ -70,26 +97,30 @@ int main(int argc, char** argv) {
     int start = rank * files_per_process + (rank < extra_files ? rank : extra_files); // Início da fatia de arquivos para este processo
     int end = start + files_per_process + (rank < extra_files ? 1 : 0); // Fim da fatia de arquivos
 
-    int* occurrences = calloc(num_files, sizeof(int)); // Vetor para armazenar o número de ocorrências por arquivo
+    int* occurrences = calloc(num_files * num_words, sizeof(int)); // Vetor para armazenar o número de ocorrências por arquivo e por palavra
     
     // Conta as ocorrências nos arquivos atribuídos a este processo
     for (i = start; i < end; i++) {
-        occurrences[i] = count_occurrences(filenames[i], search_word);
+        for (int j = 0; j < num_words; j++) {
+            occurrences[i * num_words + j] = count_occurrences(filenames[i], words[j]);
+        }
     }
 
     // Recolhe os resultados no nó raiz
     int* all_occurrences = NULL;
     if (rank == 0) {
-        all_occurrences = calloc(num_files, sizeof(int)); // Vetor para armazenar todas as ocorrências
+        all_occurrences = calloc(num_files * num_words, sizeof(int)); // Vetor para armazenar todas as ocorrências
     }
     
     // Reduz os dados (soma as ocorrências de todos os processos) no processo raiz
-    MPI_Reduce(occurrences, all_occurrences, num_files, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    MPI_Reduce(occurrences, all_occurrences, num_files * num_words, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
     if (rank == 0) {
         // Imprime os resultados no processo raiz
         for (int j = 0; j < num_files; j++) {
-            printf("Arquivo: %s, Ocorrências: %d\n", filenames[j], all_occurrences[j]);
+            for (int k = 0; k < num_words; k++) {
+                printf("Arquivo: %s, Palavra: %s, Ocorrências: %d\n", filenames[j], words[k], all_occurrences[j * num_words + k]);
+            }
         }
         free(all_occurrences); // Libera memória alocada
     }
@@ -99,5 +130,5 @@ int main(int argc, char** argv) {
     return 0;
 }
 
-//mpicc -o search_mpi search_mpi.c
-//mpirun -np 4 ./search_mpi example
+// Compilar: mpicc -o search_mpi search_mpi.c
+// Executar: mpirun -np 4 ./search_mpi "frase de busca"
